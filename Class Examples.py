ae551a3ca61fbabdc77b5d6aff203d83bb77b419
Azasoft1 <73752372+Azasoft1@users.noreply.github.com>
2020-11-01 18:52:43 +0400

    Add files via upload

---
+++ Class Examples.py
@@ -0,0 +1,877 @@
+String1= "This is the second part of print function"
+x=5
+y=12
+
+z=x+y
+
+print(String1)
+print("Hello this is our First Example")
+print("This is the first part" + String1)
+print(z)
+
+print("The result of adding\t", 5, "and", 12, "is equal to", z)
+
+
+print("        #         ###")
+print("       # #        #   #")
+print("      #   #       #  # ")
+print("     #######      # #  ")
+print("    #       #     #  #  ")
+print("   #         #    #    # ")
+print("  #           #   # # # #   ")
+print("Abedallah")
+print("you can watch the Team screena nd Type here at the same time! ")
+print("Hello", x, y, z)
+
+
+Weight=86.5
+Weight = Weight + 2
+print(Weight)
+
+Age = 38
+Age = Age + 2
+print(Age)
+
+Name="Abedallah"
+Age= 38
+Degree = "PhD"
+Location = "Sharjah"
+isMarried = True
+
+print("My name is,", Name, "\nMy age is", Age, "\nMy degree level is", Degree, "\nI am living in", Location, "\nMy martial status is", isMarried)
+
+Width = 120
+Width = Width +10
+print(Width)
+
+Weight = 85
+No_of_Plates= 3
+Weight_of_plate = 0.5
+
+Weight = Weight + No_of_Plates* Weight_of_plate
+
+print(Weight)
+
+print(type(Weight))
+Weight = "Abedallah"
+print(type(Weight))
+
+
+print("My weight is\t" + Weight + "\tHello!")
+Million = 100,000
+print(Million)
+print(type(Million))
+
+First_Name= "Abedallah"
+Family_Name = "Abulakishik"
+
+
+_Last_Name = "Abedallah"
+print(_Last_Name)
+
+
+string="abedalla"
+print(string[0])
+length =len(string)
+print("last character is")
+print(string[length-1])
+print("The middle character is",string[(length // 2)-1] , string[(length // 2)])
+
+
+#no=input("Enter a number ")
+#x= len(no)
+#print("first number is", no[0], "last number is", no[x-1])
+
+x = 1598
+print("hi", len(str(x)))
+print(int(x//1000))
+
+# solution of 2.1
+INCH=25.4
+width, hight =8.5, 11
+print("The rectangle perimeter",  2*(width+hight)*INCH)
+
+#2.7
+
+from math import pi
+
+# Read the radius from the user.
+#radius = float(input("Enter the radius: "))
+
+# Compute the area and circumference.
+#area = pi * radius * radius
+#circumference = 2 * pi * radius
+
+# compute the volume and surface area.
+#volume = 4 / 3 * pi * radius ** 3
+#surface_area = 4 * pi * radius * radius
+
+# Display the results.
+#print("The circle has area %.2f.", area)
+#print("The circle has circumference %.2f." %  circumference)
+#print("The sphere has volume %.2f." %  volume)
+#print("The sphere has surface area %.2f." %  surface_area)
+
+
+#
+line1="+---+---+---+"
+line2= "|---|---|---|"
+print("\n",line1,"\n",line2,"\n", line1,"\n", line2)
+
+
+x = 100
+print(dir(x))
+import math as mt
+print(dir(mt))
+print(help(x))
+print(help(mt))
+from math import exp as e
+print("the value of exponential is",e(10),2)
+##############################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Milage = float(input("Please user! keyin the Milage in KiloMeter"))
+#KM_TO_MILES= 0.621371
+#print("The Milage of this car in MILES=", Milage * KM_TO_MILES )
+
+# Conversion between Fahrenhit to Celesius
+# 38 C. F= ?!
+
+#Celsius = float(input("Dear USER! Please keyin the tempreature in CELESIUS to getthe results in FAHRENHITE"))
+#FAHRENHITE = (Celsius * 9/5) + 3212
+
+# print("The equivelant degree in FARHNHITE is equal to", FAHRENHITE)
+
+literPerOunce = 0.0296
+
+canVolume = 12 * literPerOunce
+print(literPerOunce)
+
+
+x= 5
+y = 9.9
+print(x+y)
+print(x**3)
+
+x=3
+y = 17
+print(y //x) # Double division means Floor division function.
+
+
+
+x= 22
+y = 1567
+print( y % x)
+
+print( abs( -12))
+print(abs(0))
+print(abs(333))
+
+print(round(123.25656988, 2))
+print(round(123.25656988, 5))
+
+
+print(min(12, 32, 43, 54, 65, 76, 2))
+print(max(12, 32, 43, 54, 65, 76, 2))
+
+import math as mt
+print(mt.pi)
+print(mt.e)
+print(mt.sqrt(25))
+print(mt.exp(4))
+print(mt.sin(180))
+
+
+print(float(3*4))
+print(int(3*4))
+print(int(3.56898 * 12.568598))
+print(float(3.56898 * 12.568598))
+####################################################
+####################################################
+
+
+
+price = 4.3556
+quantity = 100
+total = price * quantity
+# Should be 100 * 4.35 = 435.00
+print(" 151 51 51 51 The total value is % & * # @ ", total)
+
+#######################
+
+from math import pi, sin, sqrt
+print(sqrt(25))
+
+Message = ' Sultan Said: "Hello" '
+print(Message)
+
+Name="Abedallah   Zaid"
+print(len(Name))
+Name="Abedallah"
+Father_Name = "Zaid"
+print(Name, Father_Name )
+print(Name + Father_Name)
+
+
+
+Dashed= "#" *10
+print(Dashed)
+print(Dashed)
+print(Dashed)
+print(Dashed)
+
+#####################3
+
+can_size = float ("1234.55")
+
+
+
+Student_Name = "Lara"
+print(Student_Name[3])
+print(Student_Name[-1])
+print(len(Student_Name))
+print(Student_Name[len(Student_Name)-1])
+
+print(Student_Name.upper())
+
+Name="Abedallah"
+print(Name.replace("be", "BE"))
+
+
+
+from math import pi
+print(pi)
+
+#Name = float(input("Please enter yoru weight"))
+#print(Name)
+
+price_per_liter= 1.21997
+print("Price per liter %.2f" %price_per_liter)
+
+
+CANS_PER_PACK = 6  # This is a comment!
+
+#userInput = input("Please enter the price for a six-packs: ")
+#packPrice = float(userInput)
+
+#userInput = input("Please enter the volume for each can in ounce:")
+#canVolume = float(userInput)
+#packVolume = canVolume * CANS_PER_PACK
+#pricePerOunce= packPrice / packVolume
+#print("price per ounce: %12f" % pricePerOunce)
+
+
+# Floor_No = int(input("Please enter the floor number"))
+Floor_No = 30
+if Floor_No <100:
+    if Floor_No == 21:
+        print("This is not acceptable value")
+    if Floor_No>=20:
+     actual_floor=Floor_No+2
+     print("Hello")
+     print("My name is Abedallah")
+     print(" I am working at AUE")
+
+
+    else:
+        actual_floor = Floor_No
+
+    print(actual_floor)
+
+else:
+    print("Please enter a floor number that is acceptable and below 100 floors! ")
+
+
+
+
+
+
+
+
+sum11 = 10
+print(sum11-2 if sum11>8 else sum11)
+
+if sum11 == 11:
+    print("Sum11 is equal to 11")
+else:
+    print("sum11 is not equal to 11")
+
+
+string1="Abedallah"
+if string1 != "Abedallah":
+    print("Sting1 is not Equal to Abedallah")
+else:
+    print("String1 is equal to Abedallah")
+
+import math
+r= math.sqrt(2.0)
+if r*r ==2:
+    print("r*r=2")
+else:
+    print("The sqrt(2.0) is equal to", r)
+
+EPSILON=1E-14
+if abs(r*r -2.0) < EPSILON:
+    print("sqrt (2.0 squared is approximately 2.0")
+
+
+#Drink = str(input("Please order yoru Drink"))
+#if Drink =="Wine":
+ #   Age=float(input("Please Enter yoru Age"))
+  #  if Age >=21.0:
+   #     print("We can serve Wine")
+    #else:
+     #   print("According to Law and since your below 21 you can't order Wine")
+#print("Client can order any Drink!")
+
+
+print((5 * "j"))
+string3 = "%%%%%"
+print(string3*12)
+print("\n\n\n\n\n\n")
+
+print("pi=%s" % "3.14178767")
+
+s = "1200"
+t = "20"
+
+if s == t:
+    comparison = "is the same as"
+else:
+    comparison = "is not the same as"
+print("The string '%s' %s the string '%s'." % (t, comparison, t))
+
+Name = "Abedallah"
+Father = "Zaid"
+Family = "Abualkishik"
+print("My name is:'%s', and my Father name is:'%s', and my Family name is:'%s'" % (Name, Father, Family))
+print("My name is '%s  '\f' %s'" % (Name, Father))
+
+Password = "Hello"
+string5 = "hello"
+if Password != string5:
+    print("Not equal")
+
+Attending = False
+if not Attending and Password == "Hello":
+    print("YES IT WORKS")
+
+Grade = 79
+
+if Grade >= 90:
+    print("A")
+elif Grade >= 80:
+    print("B")
+elif Grade>=70:
+    print("C")
+else:
+    print("Failed")
+
+if Grade>= 90 and Grade<100:
+    print("A")
+if Grade >=80 and Grade<90:
+    print("B")
+if Grade >=70 and Grade<80:
+    print("C")
+else:
+    print("Failed")
+
+
+print(100 * "#")
+
+if Grade>= 90 and Grade<100:
+    print("A")
+if Grade >=80 and Grade<90:
+    print("B")
+if Grade >=70 and Grade<80:
+    print("C")
+else:
+    print("Failed")
+
+
+
+
+
+
+
+
+sum = 99
+if sum == 7:
+    print("Hello the condition is TRUE")
+else:
+    print("The condition is FALSE")
+
+
+
+
+
+print(sum-2 if sum>8 else sum)
+
+
+if sum >= 7:
+    print(sum-2)
+else:
+    print(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+Name = "abedallah"
+Doctor_Name = "Abedallah"
+
+if Name != Doctor_Name:
+    print("The condition is TRUE")
+else:
+    print("The condition is FALSE")
+
+
+
+x = sqrt(2)
+y = 2.0
+
+if x * x == y:
+   print("sqrt(2) times sqrt(2) is 2")
+else :
+   print("sqrt(2) times sqrt(2) is not two but %.18f" % (x * x))
+
+
+
+#date = str(input("Please key in teh date! "))
+#price = float(input("Please keyin the item price:"))
+#if date == "10.24":
+ #   if price >=128:
+  #      price = price * 84/100
+   # else:
+    #    price = price * 92/100
+    #print("The price of the item after discount is", price)
+#else:
+#    print("There is no disount offered Today!")
+
+
+Grade =99
+if Grade>= 90:
+    print("A")
+if Grade >=80 :
+    print("B")
+if Grade >=70 :
+    print("C")
+if Grade >=60:
+    print("D")
+else:
+    print("Failed")
+######################################################
+######################################################
+RATE1 = 0.1  # Percentage of Tax below the threshold
+RATE2 = 0.25
+
+Single_Threshold= 32000.0
+Married_Threshold = 64000.0
+
+#income = float(input("Please enter your annual income!"))
+#Martial_Status = str(input("Please enter yoru martial status! 'M' for Married: 'S' for Single! "))
+
+Tax1=0
+Tax2=0
+
+#if Martial_Status== "S":
+#    if income<= Single_Threshold:
+#        Tax1 = Single_Threshold*RATE1
+#    else:
+#        Tax1= income*RATE1
+#        Tax2= RATE2 * (income-Single_Threshold)
+#else:
+#    if income <= Married_Threshold:
+#        Tax1 = income * RATE1
+#    else:
+#        Tax1 = Married_Threshold * RATE1
+#        Tax2 = RATE2 * (income - Married_Threshold)
+#print("Your annulae tax is", float(Tax1+Tax2))
+
+
+
+#Magnitude = float(input("Please enter the mangitude of teh earthquick!"))
+#if Magnitude>= 8.0:
+#    print("Most structures fall")
+#elif Magnitude>= 7.0:
+#    print("Many building destroyed")
+#elif Magnitude>=6.0:
+#    print("Many buildings considerably damaged, some collapse")
+#elif Magnitude>= 4.5:
+#    print("Damage to poorly constructed buildings")
+#else:
+#    print("No destruction of buildings")
+
+
+#if Magnitude>= 8.0:
+#    print("Most structures fall")
+#if Magnitude>= 7.0 and Magnitude<8.0:
+#    print("Many building destroyed")
+#if Magnitude>=6.0 and Magnitude<7.0:
+#    print("Many buildings considerably damaged, some collapse")
+#if Magnitude>= 4.5 and Magnitude<6.0:
+#    print("Damage to poorly constructed buildings")
+#else:
+#    print("No destruction of buildings")
+
+
+Flag= True
+Flag2 = True
+Flag3 = True
+#Temp = int(input("Please enter the Tempreature!"))
+#if Temp <= 0 or Temp>= 100:
+#    print("Not Liqiud")
+
+#if Temp >=0 and Temp <=100:
+ #   print("Its LIQUID!")
+
+
+##########################################################################
+#if not Flag or Temp>=0 or not Flag2 and Flag3:
+#    print("The Flag is True and the Temp is above 0")
+#else:
+#    print("One of teh two conditions are FALSE")
+
+Balance= 10000
+Year=1
+Interest=0.05
+Target=20000
+
+while Balance <= Target:
+    Balance = Balance + Balance*Interest
+    Year = Year + 1
+    print("The new value of Balance is", Balance)
+
+print("Its takes", Year, "To double the 10000$!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Temperature  = 88
+if Temperature  <= 0 or Temperature >= 100:
+   print("Not Liquid")
+else:
+   print("Its Liquid!")
+
+
+Flag1= False
+Flag2 = True
+Flag3 = True
+
+if not Flag1 and Temperature >=0 or not Flag2 and Flag3:
+  print("The Flags are True and the Temperature  is above 0")
+else:
+    print("The Flags are False and the Temperature  is below 0")
+
+
+
+
+sum = 0
+counter=0
+while counter<=100:
+    if(counter % 2 == 0):
+        print(counter)
+        sum = sum + counter
+    counter=counter+1
+print(len(str(sum)))
+
+#number= int(input("Please enter a number"))
+
+#if number <0:
+#    number= number * -1
+#print(len(str(number)))
+
+
+#string1 = str(input("Enter any string"))
+#string2 = str(input("Enter any string"))
+#string3 = str(input("Enter any string"))
+#if ord(string3[0]) <= ord(string1[0]) and ord(string3[0]) <=ord(string2[0]):
+#    print(string3)
+
+
+
+
+
+# num = int(input("Please enter a number"))
+
+#digit=0
+
+#while num > 0 or num < 0:
+
+#   digit = digit + 1
+
+#   num = num//10
+
+# print("the number you enterd has a", digit,"digit")
+
+
+#valid = False
+#while not valid:
+#  value = int(input("Please enter a positive value < 100: "))
+#  if value > 0 and value < 100 :
+#      valid = True
+#   else :
+#      print("Invalid input.")
+
+
+
+#Negative=0
+#count=0
+#number = input("Please enter a number:")
+#while number !="":
+#    number= int(number)
+#    if number <0:
+#        count=count+1
+#    number = input("I am within the while loop. Please enter a new number:")
+#print("The number of negative enteries was",  count)
+
+
+#largest = int(input("Enter a value for Largest number: "))
+#inputStr = input("Enter a value: ")
+#while inputStr != "":
+#   value = int(inputStr)
+#   if value < largest:
+#      largest = value
+#   inputStr = input("Enter a value: ")
+
+#print("largest value is", largest)
+
+
+Name ="ABEDALLAH"
+i = 0
+while i < len(Name):
+     print(Name[i])
+     i = i+1
+
+print("#" * 100)
+for i in Name:
+    print(i)
+
+
+#for i in range(1, 6):
+ #   print(i, end="               ")
+
+
+
+for i in range(0, 11):
+    for j in range(0, i):
+        print("*", end="")
+    print("\r")
+
+print( "hello", "\n", "\r", "world")
+
+
+
+NROWS=10
+NCOLS=5
+
+for i in range(0, NCOLS):
+    print("%10d" % i, end="")
+print()
+for j in range(0, NCOLS):
+    print("%10s" % "X", end="" )
+print()
+print("#####" * 10)
+
+
+for i in range(0, NROWS+1):
+    for j in range(0, NCOLS):
+        print("%10d" % j,  end="" )
+    print()
+
+
+
+for i in range(0, 11):
+    for j in range(0, i):
+        if j %2 ==0:
+            print("*", end="")
+        if j%2==1:
+            print("#", end="")
+    print("\r")
+###########################################
+###########################################
+###########################################
+for i in range (1, 11):
+    if i%2==0:
+        for j in range(1,6):
+            print("#", end="")
+        print()
+    if i%2==1:
+        for j in range(1,5):
+            print("@", end="")
+        print()
+
+
+#Morestudents =True
+#while Morestudents:
+#    for j in range (0, 2):
+#        exam_grade=int(input("enter the grade of the exam"))
+#        total=total+exam_grade
+#    New_student = input("Do you have grades for new students? True/False")
+#    if New_student=="True":
+#        Morestudents=True
+#    if New_student=="False":
+#        Morestudents=False
+
+#print("Outside loop")
+
+count=0
+name="AbedaLLah"
+for char in name:
+    if char.isupper():
+        count=count+1
+print(count)
+
+#text=str(input("input any text"))
+#for i in range(len(text)):
+#    if text[i].isupper():
+#        print(i)
+
+x="Hello 3 this1"
+Found = False
+position=0
+while not Found and position <len(x):
+    if x[position].isdigit():
+        position = position+1
+        print(position)
+    else:
+        position=position+1
+
+
+for i in range(len(x)):
+    if x[-i].isdigit():
+        print(len(x) -i)
+
+
+x=10
+if x != 10:
+    print("X is not 10")
+else:
+    print("X is 10")
+
+#from random import randint
+#for i in range(1,1000000):
+#    r1 = randint(1,1000)
+#    r2 = randint(1,1000)
+#    print("The values of R1 and R2 at the", i,"th iteration respectively are:", r1, "\t", r2)
+#    print("\r")
+
+def boxString(contents) :
+    n = len(contents)
+    if n == 0 :
+        return # Return immediately
+    print("-" * (n + 2))
+    print("!" + contents + "!")
+    print("-" * (n + 2))
+boxString("Abedallah")
+
+
+def cubevolume (side):
+    return side**3
+
+print(cubevolume((100)))
+yy=cubevolume(10)
+print(yy)
+
+def rectangle_area(x,y):
+    return x*y
+
+
+def main():
+    print(cubevolume(333))
+    print(rectangle_area(83,83))
+
+main()
+
+##########################################
+##########################################
+##########################################
+##########################################
+##########################################
+
+NO_Rows= 10
+No_Cols= 4
+
+for i in range (1, No_Cols+1):
+    print("%10d" % i, end="")
+print()  # will print a newl line.
+for i in range (1, No_Cols+1):
+    print("%10s" % "X", end="")
+
+# Nest for loop to print teh Table contents.
+print()
+for i in range(1, NO_Rows+1):   # This loop will itertae 10 times
+    for j in range(1, No_Cols+1):   # This loop will iterate  4 times
+        print("%10.0f" % i**j, end="")
+    print()
+
+
+for i in range(1, 4):
+    for j in range (1, 5):
+        print("*", end="")
+    print()
+
+print("#" * 33)
+for i in range(1, 5):
+    for j in range (1, 4):
+        print("*", end="")
+    print()
+print("$$$$$" *100)
+
+
+
+for i in range(1, 5): # At the value of 2
+    for j in range (1, i+1):
+        print("*", end="")
+    print()
+
+
+print("$$$$$$$$$$$$$$$$$" *100)
+
+for i in range(11, 1, -1): # 5,  3, 1
+    for j in range (0, i-1):
+        print("*", end="")
+    print()
+
+
+
+
+
+
+
+
+
+
+
